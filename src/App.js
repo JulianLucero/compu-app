d4be5c295bc60e6b0c0a222b8a0cad32b00c1777
JulianLucero <julian.eduardo.lucero@gmail.com>
2021-06-08 12:07:02 -0300

    CartWidget

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
+import ItemListContainer from "./components/ItemListContainer";
+import CartWidget from "./components/CartWidget";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <CartWidget/>
+      <ItemListContainer greeting = "Hola aca va estar mi catalogo de Compu Shopping"/>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react'
-import {Navbar} from 'react-bootstrap'
+import React, { Component } from 'react';
+import {Navbar, Nav} from 'react-bootstrap';
+import CartWidget from './CartWidget';
 
 class NavBar extends Component{
     render () {
     return (
         <Navbar bg="Ligth">
             <Navbar.Brand href="#home">CompuShooping</Navbar.Brand>
-            <Navbar.Brand href="#about">About</Navbar.Brand>
-            <Navbar.Brand href="#contact">Contact</Navbar.Brand>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#productos">Productos</Nav.Link>
+            <Nav.Link href="#contacto">Contacto</Nav.Link>
+            <CartWidget/>
         </Navbar>
     )
-}
+};
 }
 
 export default NavBar;
