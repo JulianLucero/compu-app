0664f88cad8d90061cbdf02d53dfd5d7298cbb76
JulianLucero <julian.eduardo.lucero@gmail.com>
2021-06-17 17:12:54 -0300

    ItemDetail- ItemDetailContainer

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -72,7 +72,7 @@ function ItemList() {
     //si llegamos a esta intancia, significa que tuvimos un error,por eso "rechazamos" (reject) la promesa.
     reject("Error obteniendo los datos :(");
     }, 
-    2000);
+    50);
   });  
 
   //una vez resuelta la promesa... 
