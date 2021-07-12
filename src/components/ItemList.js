31a24330d6b4e76394d3be24090181c04487aec7
JULIAN <40304605+JulianLucero@users.noreply.github.com>
2021-07-12 20:29:01 -0300

    Firebase v2

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -10,7 +10,7 @@ import {useParams} from 'react-router-dom';
         precio: 35000,
         category: "Monitores",
         stock: 23,
-        id: 1,
+        id: "KcULXlBY2OB8XUbGIgqL",
         name: "ROG - Republic of Gamers ",
         description: "Asus",
         thumbnailUrl: "https://www.asus.com/media/Odin/websites/global/ProductLine/20200813114257/P_setting_xxx_0_90_end_185.png"
@@ -19,7 +19,7 @@ import {useParams} from 'react-router-dom';
         precio: 95300,
         category: "Placa base",
         stock: 27,
-        id: 2,
+        id: "mOS1aAAOpNXH5WvRC8M3",
         name: "TUF GAMING B550M (WI-FI) ZAKU II EDITION",
         description: "Gaming",
         thumbnailUrl: "https://dlcdnwebimgs.asus.com/gain/17023a72-9fd6-42d9-bc81-40f2cb3f304b/w800"
@@ -28,7 +28,7 @@ import {useParams} from 'react-router-dom';
         precio: 150000,
         category: "Placa base",
         stock: 35,
-        id: 3,
+        id: "rq8zEYl4xga6lvz9oet6",
         name: "Z590 WIFI GUNDAM EDITION",
         description: "Gaming",
         thumbnailUrl: "https://dlcdnwebimgs.asus.com/gain/f1cd5a2d-ec56-4949-8f1c-2274692c621e/w800"
@@ -37,7 +37,7 @@ import {useParams} from 'react-router-dom';
         precio: 5000,
         category: "Mouse Pad",
         stock: 123,
-        id: 4,
+        id: "p1en7DEocetc6wRESmTf",
         name: "Mouse pad Asus TUF Gaming P3",
         description: "Gaming",
         thumbnailUrl: "https://dlcdnwebimgs.asus.com/gain/14da7e79-c9d4-4973-97d2-fec63e82092c/w800"
@@ -46,7 +46,7 @@ import {useParams} from 'react-router-dom';
         precio: 180000,
         category: "Placa de video",
         stock: 17,
-        id: 5,
+        id: "iOiWdqKFf2vsvFKYVo8q",
         name: "ROG STRIX LC",
         description: "Gaming",
         thumbnailUrl: "https://www.asus.com/microsite/Graphics-Cards/radeon-rx-6000-series/latin/img/banner/banner-lc-2.png"
@@ -55,7 +55,7 @@ import {useParams} from 'react-router-dom';
         precio: 30000,
         category: "Fuente",
         stock: 12,
-        id: 6,
+        id: "9KNV0LmKdwoizxIC2Tzk",
         name: "ROG-STRIX-1000G",
         description: "Gaming",
         thumbnailUrl: "https://dlcdnwebimgs.asus.com/gain/96F8E49A-C435-42CF-AFA5-AF00D417679E/w1000/h732"
